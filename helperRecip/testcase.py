5be644215fe89440a8337afc6422cd91a1d92dd1
dtzinov <diana@reciprocitylabs.com>
2013-06-21 20:22:40 -0500

    Update testcase.py

--- helperRecip/testcase.py
+++ helperRecip/testcase.py
@@ -13,6 +13,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support.ui import WebDriverWait
 import time, unittest
 #from selenium.webdriver import ChromeOptions
+from os.path import expanduser 
 import config
                                                                                                                                                                                                                                                                                 
 #from recip import config

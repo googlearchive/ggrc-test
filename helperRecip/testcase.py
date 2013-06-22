9448594d432f9c14c56595a9e5807d00a12ca819
dtzinov <diana@reciprocitylabs.com>
2013-06-22 00:04:24 -0500

    Update testcase.py

--- helperRecip/testcase.py
+++ helperRecip/testcase.py
@@ -1,47 +1,20 @@
 import os, sys, re
 
-#import re,sys
-
-#import com.thoughtworks.selenium.
-
-
 from unittest import TestCase
-#from selenium import selenium
 from selenium import webdriver
-from selenium.webdriver import Chrome
+from selenium.webdriver.remote.webdriver import WebDriver as RemoteWebDriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support.ui import WebDriverWait
 import time, unittest
-#from selenium.webdriver import ChromeOptions
 from os.path import expanduser 
 import config
-                                                                                                                                                                                                                                                                                
-#from recip import config
-
-
 
 class WebDriverTestCase(TestCase):
 
-
-   
     def setup(self):
         browser = config.browser
-        if browser == "firefox":
-            self.profile = webdriver.firefox.firefox_profile.FirefoxProfile()
-            self.profile.native_events_enabled
-            self.driver = webdriver.Firefox(self.profile)
-            self.profile_path = self.profile.path 
-        elif browser =="chrome":
-            home = expanduser("~")
-            chrome_driver_filename = home + "/bin/chromedriver"
-            self.driver = webdriver.Chrome(chrome_driver_filename)
-            
-            #options=ChromeOptions()
-            #options.add_argument("----display=:99")
-            #self.driver = webdriver.Chrome('/usr/local/bin/chromedriver', chrome_options=options)   
-        #self.profile = webdriver.Firefox.firefox_profile(self).FirefoxProfile
-        #self.profile.native_events_enabled
-        #self.driver = webdriver.Firefox(self.profile)
+        if browser =="chrome":
+            self.driver = RemoteWebDriver("http://ci.reciprocitylabs.com:4444/wd/hub", DesiredCapabilities.CHROME);
         self.base_url =config.url 
         time.sleep(5)
         self.driver.get(self.base_url)  
@@ -50,4 +23,3 @@ class WebDriverTestCase(TestCase):
         
     def tearDown(self):
         self.driver.quit()
- 

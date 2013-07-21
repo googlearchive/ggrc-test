336a9ddc80f7c86ff115d979efb03f364f21373e
dtzinov <diana@reciprocitylabs.com>
2013-07-21 13:32:13 -0600

    suite for all tests

---
+++ SuiteAllTests.py
@@ -0,0 +1,13 @@
+'''
+Created on Jul 21, 2013
+
+@author: diana.tzinov
+'''
+
+import TestLogin
+from Create.TestContractCreate import TestContractCreate
+from Create.TestPolicyCreate import TestPolicyCreate
+from Create.TestProgramCreate import TestProgramCreate
+from Create.TestRegulationCreate import TestRegulationCreate
+from Edit.TestProgramEdit import TestProgramEdit
+

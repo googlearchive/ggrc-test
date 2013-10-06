04cda59e2107e063a3bba5b82601f0d0bc087d6a
Diana Tzinov <diana.tzinov@Diana-Tzinov.local>
2013-10-06 11:09:07 -0600

    adding configuration option for testing against multiple environments

--- config.py
+++ config.py
@@ -4,16 +4,21 @@ Created on Feb 11, 2013
 @author: diana.tzinov
 '''
 from os.path import expanduser 
+import sys, os
+
 # the possible environment values are "STAGE", "USER"   "PRODUCTION","INTEGRATION",
 
-url = " http://grc-test.appspot.com/"
+#print os.environ['PYTHONPATH'].split(os.pathsep)
+
+url= os.getenv('TEST_SITE_URL', "http://grc-test.appspot.com/")
+print url
 environment = ""
 user = ""
 browser = "chrome"
 #browser = "firefox"
 username ="testrecip@gmail.com"
 password = "testrecip1"
-use_remote_webdriver = True
+use_remote_webdriver = False
 remote_webdriver_url = "http://ci.reciprocitylabs.com:4444/wd/hub"
 jasmine_url = "file:///Users/diana.tzinov/Downloads/jasmine/jasmine-all-good.htm"
 chrome_driver_filename = expanduser("~") + "/bin/chromedriver"

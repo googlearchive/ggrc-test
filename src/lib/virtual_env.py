12130df8d17043212da94ce6961a9a1b99e6dedb
Jernej Makovsek <jernej.makovsek@gmail.com>
2015-09-25 18:01:50 +0200

    Modifies how virtual environment path is built.

--- src/lib/virtual_env.py
+++ src/lib/virtual_env.py
@@ -2,5 +2,8 @@ from lib import constants
 
 
 def activate(project_root_path):
-    activate_this_path = project_root_path + constants.path.VIRTUALENV_ACTIVATE
+    activate_this_path = project_root_path \
+        + constants.path.VIRTUALENV_DIR \
+        + constants.path.BIN_DIR \
+        + constants.path.VIRTUALENV_ACTIVATE
     execfile(activate_this_path, dict(__file__=activate_this_path))

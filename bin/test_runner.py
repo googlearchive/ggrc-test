3ef11173b0fddd45654b4e125a2d4a8dfb874ea5
Miha Zidar <zidarsk8@gmail.com>
2015-09-29 15:16:43 +0200

    Cleanup testrunner
    - Install requirements with -r flag.
    - Make test runner runnable from any directory.

--- bin/test_runner.py
+++ bin/test_runner.py
@@ -26,22 +26,11 @@ def set_up_directories():
     file_ops.create_directory(VIRTENV_PATH)
 
 
-def _install(package_name):
-    # using subprocess instead of pip.main(['install', package_name]) because subprocess installs the package
-    #  in the right virtenv which we have previously activated
-    exit_code = subprocess.call(["pip", "install", package_name])
+def update_virtenv():
+    exit_code = subprocess.call(["pip", "install", "-r", constants.path.REQUIREMENTS])
 
     if exit_code != 0:
-        raise EnvironmentError("Problem installing library=%s. Please check the logs." % package_name)
-
-
-def update_virtenv():
-    with open(PROJECT_ROOT_PATH + constants.path.REQUIREMENTS) as f:
-        lines = f.readlines()
-
-    for package_name in lines:
-        logger.info("Updating or installing %s" % package_name)
-        _install(package_name)
+        raise EnvironmentError("Problem installing requirements")
 
 
 def run_tests():
@@ -53,7 +42,7 @@ if __name__ == "__main__":
     set_up_directories()
     log.set_default_file_handler(logger, constants.path.LOGS + constants.log.Logger.TEST_RUNNER)
 
-    exit_code, result = commands.getstatusoutput("virtualenv %s" % constants.path.VIRTUALENV_DIR)
+    exit_code, result = commands.getstatusoutput("virtualenv %s" % PROJECT_ROOT_PATH + constants.path.VIRTUALENV_DIR)
 
     if exit_code != 0:
         print result

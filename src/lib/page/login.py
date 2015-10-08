28a588313f60d9dba5167e187add1367c5140066
Jernej Makovsek <jernej.makovsek@gmail.com>
2015-10-08 04:42:06 +0200

    Add login page

--- src/lib/page/login.py
+++ src/lib/page/login.py
@@ -1,11 +1,12 @@
-from lib.page.base import BasePage
+from lib.constants.element.landing_page import login_button
+from lib.base import Page
 from lib import constants
 
 
-class LoginPage(BasePage):
+class LoginPage(Page):
     def __init__(self, driver):
         super(LoginPage, self).__init__(driver)
-        self._driver.get(constants.uri.LOGIN)
+        self._driver.get(constants.uri.BASE)
 
     def login(self, username=None, password=None):
-        self._driver.find_element_by_css_selector("a.btn.btn-large.btn-info").click()
+        self._driver.find_element_by_css_selector(login_button.SELECTOR).click()

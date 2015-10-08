cdb0e82b739d679989c0e56a4afae3e7ea7f8908
Jernej Makovsek <jernej.makovsek@gmail.com>
2015-10-08 20:02:48 +0200

    Resolve potential import clashes

--- src/lib/log.py
+++ src/lib/log.py
@@ -3,7 +3,7 @@ from lib import constants
 
 
 def set_default_file_handler(logger, log_path):
-    formatter = logging.Formatter(constants.log.Logger.DEFAULT_FORMAT)
-    fh = logging.FileHandler(filename=log_path, mode=constants.file.Mode.WRITE)
+    formatter = logging.Formatter(constants.log.DEFAULT_FORMAT)
+    fh = logging.FileHandler(filename=log_path, mode="w")
     fh.setFormatter(formatter)
     logger.addHandler(fh)

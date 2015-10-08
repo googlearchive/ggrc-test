dcef5b9af1cdc907400a223cb462af9be4fd5ead
Jernej Makovsek <jernej.makovsek@gmail.com>
2015-10-08 20:03:44 +0200

    Update logger's constants reference

--- src/lib/environment/__init__.py
+++ src/lib/environment/__init__.py
@@ -5,13 +5,15 @@ from lib import constants, file_ops
 
 PROJECT_ROOT_PATH = os.path.dirname(os.path.abspath(__file__)) + "/../../../"
 VIRTENV_PATH = PROJECT_ROOT_PATH + constants.path.VIRTUALENV_DIR
-CHROME_DRIVER_PATH = PROJECT_ROOT_PATH + constants.path.RESOURCES + constants.path.CHROME_DRIVER
-CONFIG_FILE_PATH = PROJECT_ROOT_PATH + constants.path.RESOURCES + constants.path.YAML
+CHROME_DRIVER_PATH = PROJECT_ROOT_PATH + constants.path.RESOURCES \
+                     + constants.path.CHROME_DRIVER
+CONFIG_FILE_PATH = PROJECT_ROOT_PATH + constants.path.RESOURCES \
+                   + constants.path.YAML
 yaml = file_ops.load_yaml_contents(CONFIG_FILE_PATH)
 LOGGING_FORMAT = yaml[constants.yaml.LOGGING][constants.yaml.FORMAT]
 
 # register loggers
-selenium_logger = logging.getLogger(constants.log.Selenium.SELENIUM_REMOTE_CONNECTION)
+selenium_logger = logging.getLogger(constants.log.SELENIUM_REMOTE_CONNECTION)
 
 # Only display possible problems
 selenium_logger.setLevel(logging.WARNING)

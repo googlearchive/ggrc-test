85d6c591418cd52bffe7a324770e0609a0fcb7b9
Jernej Makovsek <jernej.makovsek@gmail.com>
2015-11-30 16:16:33 +0100

    Add custom exception

---
+++ src/lib/exception.py
@@ -0,0 +1,12 @@
+# Copyright (C) 2015 Google Inc., authors, and contributors <see AUTHORS file>
+# Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+# Created By: jernej@reciprocitylabs.com
+# Maintained By: jernej@reciprocitylabs.com
+
+
+class ElementNotFound(Exception):
+    pass
+
+
+class DocstringsMissing(Exception):
+    pass

cd2e2e319a4cad65c72d1423406cc3988cd3a0c8
Jernej Makovsek <jernej.makovsek@gmail.com>
2015-11-30 16:18:36 +0100

    Add program page tests

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,4 @@
-# -*- coding: utf-8 -*-
+# Copyright (C) 2015 Google Inc., authors, and contributors <see AUTHORS file>
+# Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+# Created By: jernej@reciprocitylabs.com
+# Maintained By: jernej@reciprocitylabs.com

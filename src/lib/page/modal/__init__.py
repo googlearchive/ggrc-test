7c54da399a4902a069c9d0cb9688c8cc2514ee36
Jernej Makovsek <jernej.makovsek@gmail.com>
2015-11-30 16:14:04 +0100

    Add modals

---
+++ src/lib/page/modal/__init__.py
@@ -0,0 +1,34 @@
+# Copyright (C) 2015 Google Inc., authors, and contributors <see AUTHORS file>
+# Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+# Created By: jernej@reciprocitylabs.com
+# Maintained By: jernej@reciprocitylabs.com
+
+from lib.page.modal import (
+    new_access_group,
+    new_audit,
+    new_clause,
+    new_contract,
+    new_control,
+    new_controll_assessment,
+    new_data_asset,
+    new_facility,
+    new_issue,
+    new_market,
+    new_objective,
+    new_org_group,
+    new_person,
+    new_policy,
+    new_process,
+    new_product,
+    new_program,
+    new_project,
+    new_regulation,
+    new_request,
+    new_risk,
+    new_section,
+    new_standard,
+    new_system,
+    new_threat,
+    new_vendor,
+    new_workflow
+)

2cc26ca1305eb29f397f62142dfc8ab19a90207b
Jernej Makovsek <jernej.makovsek@gmail.com>
2015-11-30 16:14:25 +0100

    Refactor dashboard page

--- src/lib/page/dashboard.py
+++ src/lib/page/dashboard.py
@@ -1,7 +1,14 @@
-from lib import constants
+# Copyright (C) 2015 Google Inc., authors, and contributors <see AUTHORS file>
+# Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+# Created By: jernej@reciprocitylabs.com
+# Maintained By: jernej@reciprocitylabs.com
+
+from lib import constants, environment
 from lib.page import widget_bar, header
 
 
 class DashboardPage(widget_bar.WidgetBarPage, header.HeaderPage):
-    def navigate_to(self):
-        self._driver.get(constants.uri.BASE + constants.uri.DASHBOARD)
+    URL = environment.APP_URL + constants.url.DASHBOARD
+
+    def __init__(self, driver):
+        super(DashboardPage, self).__init__(driver)
